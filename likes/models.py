d1476c50649b2cbfda8d7855518018c48ff635a4
DIO23521 <romashko.dima2003@gmail.com>
2026-01-20 18:03:00 +0100

    *

--- likes/models.py
+++ likes/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 class LikedItems(models.Model):
-    user = models.ForeignKey(User, )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Эти 3 обьекта нужны для определения generic relationships
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
