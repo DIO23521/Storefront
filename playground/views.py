d96d7b60e1ac86b5eeab8183081e0a6f2061103c
DIO23521 <romashko.dima2003@gmail.com>
2026-01-29 22:55:14 +0100

    Retrieving Objects

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
+
 
 # Create your views here.
 # request -> response
@@ -13,12 +15,16 @@ from store.models import Product
 # e.t.c
 #       ⬇️
 def say_hi(request):
-    query_set = Product.objects.all()
+#    query_set = Product.objects.all()
+#    query_set.filter().filter().order_by() <-- complex query
 
-# query_set.filter().filter().order_by() <-- complex query
+#    product = Product.objects.get(pk=0) <- нужно обернуть в "try"
+#    product = Product.objects.filter(pk=0).first()       *<-- ways to retreat data
+    exists = Product.objects.filter(pk=0).exists()
 
-    for product in query_set:
-        print(product)
+
+#    for product in query_set:
+#        print(product)
 
     return render(request, 'hello.html', {'name': 'Dima'})
 
